315fa83a974db72fffdc6f512d51e56c8bb939be
Mukesh Kaushal <mukesh@shinewebservices.com>
2021-09-24 13:41:26 +0530

    changes

--- src/web/pages/Appointment/BookAppointment/BookAppointment.js
+++ src/web/pages/Appointment/BookAppointment/BookAppointment.js
@@ -179,7 +179,7 @@ const BookAppointment = (props) => {
                 setFormData({ ...formData, date: new Date(data.enabledDates[0]) })
                 updateOther(data.enabledDates[0], 3, "date");
               },
-              0
+              100
             );
           }
           else {
@@ -189,7 +189,7 @@ const BookAppointment = (props) => {
                 setFormData({ ...formData, date: "" });
                 updateOther("", 3, "date");
               },
-              0
+              100
             );
 
           }
